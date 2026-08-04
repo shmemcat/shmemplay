6284c845a85d79a1f4dedb85b56ee8f36137c521
shmemcat <emilycatellier@gmail.com>
2026-08-04 00:34:48 -0700

    Dark mode

--- app/src/main/java/io/github/shmemcat/shmemplaylist/ui/theme/Theme.kt
+++ app/src/main/java/io/github/shmemcat/shmemplaylist/ui/theme/Theme.kt
@@ -1,15 +1,34 @@
 package io.github.shmemcat.shmemplaylist.ui.theme
 
+import android.app.Activity
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
-import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.platform.LocalView
+import androidx.core.view.WindowCompat
 
 @Composable
 fun ShmemplaylistTheme(content: @Composable () -> Unit) {
+    val darkTheme = isSystemInDarkTheme()
+    val view = LocalView.current
+    if (!view.isInEditMode) {
+        SideEffect {
+            val window = (view.context as Activity).window
+            window.statusBarColor = Color.Black.toArgb()
+            window.navigationBarColor = Color.Black.toArgb()
+            WindowCompat.getInsetsController(window, view).apply {
+                isAppearanceLightStatusBars = false
+                isAppearanceLightNavigationBars = false
+            }
+        }
+    }
     MaterialTheme(
-        colorScheme = if (isSystemInDarkTheme()) darkColorScheme() else lightColorScheme(),
+        colorScheme = if (darkTheme) darkColorScheme() else lightColorScheme(),
         content = content,
     )
 }
